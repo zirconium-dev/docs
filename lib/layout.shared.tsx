5fd8da1641587b0b72781705fc2ade9cae2351ae
Tulip Blossom <tulilirockz@outlook.com>
2026-03-24 03:35:37 -0300

    init: first commit!
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>
    Co-authored-by: Synby <107513274+Synby@users.noreply.github.com>

---
+++ lib/layout.shared.tsx
@@ -0,0 +1,10 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+export function baseOptions(): BaseLayoutProps {
+  return {
+    githubUrl: "https://github.com/zirconium-dev/zirconium-docs",
+    nav: {
+      title: 'Zirconium Docs',
+    },
+  };
+}
