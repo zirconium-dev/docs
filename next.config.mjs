e38509c025dc03e35c7956c167f292bc64450a20
Tulip Blossom <tulilirockz@outlook.com>
2026-03-24 03:49:13 -0300

    fix: wrong assets path
    Signed-off-by: Tulip Blossom <tulilirockz@outlook.com>

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,16 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
+  trailingSlash: true,
   reactStrictMode: true,
+  assetPrefix: isProd
+    ? `https://zirconium-dev.github.io/docs/`
+    : "",
 };
 
 export default withMDX(config);
